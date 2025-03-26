46ccbb25ee69b0e2e84ffad703623294e99b27ef
Konstanty Silwanowicz <kosto4ka1035@gmail.com>
2025-03-26 18:16:45 +0100

    initial commit

--- src/js/script.js
+++ src/js/script.js
@@ -1,98 +1,120 @@
 /* global Handlebars, utils, dataSource */ // eslint-disable-line no-unused-vars
 
-
-  'use strict';
+'use strict';
 
 const select = {
   templateOf: {
     menuProduct: "#template-menu-product",
+  },
+  containerOf: {
+    menu: '#product-list',
+    cart: '#cart',
+  },
+  all: {
+    menuProducts: '#product-list > .product',
+    menuProductsActive: '#product-list > .product.active',
+    formInputs: 'input, select',
+  },
+  menuProduct: {
+    clickable: '.product__header',
+    form: '.product__order',
+    priceElem: '.product__total-price .price',
+    imageWrapper: '.product__images',
+    amountWidget: '.widget-amount',
+    cartButton: '[href="#add-to-cart"]',
+  },
+  widgets: {
+    amount: {
+      input: 'input[name="amount"]',
+      linkDecrease: 'a[href="#less"]',
+      linkIncrease: 'a[href="#more"]',
     },
-    containerOf: {
-      menu: '#product-list',
-      cart: '#cart',
-    },
-    all: {
-      menuProducts: '#product-list > .product',
-      menuProductsActive: '#product-list > .product.active',
-      formInputs: 'input, select',
-    },
-    menuProduct: {
-      clickable: '.product__header',
-      form: '.product__order',
-      priceElem: '.product__total-price .price',
-      imageWrapper: '.product__images',
-      amountWidget: '.widget-amount',
-      cartButton: '[href="#add-to-cart"]',
-    },
-    widgets: {
-      amount: {
-        input: 'input[name="amount"]',
-        linkDecrease: 'a[href="#less"]',
-        linkIncrease: 'a[href="#more"]',
-      },
-    },
-  };
+  },
+};
 
-  const classNames = {
-    menuProduct: {
-      wrapperActive: 'active',
-      imageVisible: 'active',
-    },
-  };
+const classNames = {
+  menuProduct: {
+    wrapperActive: 'active',
+    imageVisible: 'active',
+  },
+};
 
-  const settings = {
-    amountWidget: {
-      defaultValue: 1,
-      defaultMin: 0,
-      defaultMax: 10,
-    }
-  };
+const settings = {
+  amountWidget: {
+    defaultValue: 1,
+    defaultMin: 0,
+    defaultMax: 10,
+  }
+};
 
-  const templates = {
-    menuProduct: Handlebars.compile(document.querySelector(select.templateOf.menuProduct).innerHTML),
-  };
+const templates = {
+  menuProduct: Handlebars.compile(document.querySelector(select.templateOf.menuProduct).innerHTML),
+};
 
-  class Product{
-    constructor(){
-      const thisProduct = this;
-  
-      console.log('new Product:', thisProduct);
-    }
+class Product {
+  constructor(id, data) {
+    const thisProduct = this;
+
+    // Przypisanie argumentów do instancji
+    thisProduct.id = id;
+    thisProduct.data = data;
+
+    console.log('new Product:', thisProduct);
+
+    // Wywołanie metody renderInMenu po stworzeniu instancji
+    thisProduct.renderInMenu();
   }
 
-  
+  renderInMenu() {
+    const thisProduct = this;
 
-  const app = {
+    // Generowanie HTML
+    const generatedHTML = templates.menuProduct(thisProduct.data);
+
+    // Tworzenie elementu DOM
+    thisProduct.element = utils.createDOMFromHTML(generatedHTML);
+
+    // Znalezienie kontenera menu
+    const menuContainer = document.querySelector(select.containerOf.menu);
+
+    // Dodanie stworzonego elementu na stronę
+    menuContainer.appendChild(thisProduct.element);
+  }
+}
+
+
+
+const app = {
+
+  initMenu: function () {
+    const thisApp = this;
     
-    initMenu: function () {
-      const testProduct = new Product();
-      console.log('testProduct:', testProduct);
-    },
+    console.log('thisApp.data:', thisApp.data); // Sprawdzenie czy dane są wczytane poprawnie
+    
+    for (let productId in thisApp.data.products) {
+      new Product(productId, thisApp.data.products[productId]); // Tworzenie instancji produktu
+    }
+  },
 
-    initData: function(){
-      const thisApp = this;
-  
-      thisApp.data = dataSource;
-      console.log('thisApp.data:', thisApp.data);
-    },
+  initData: function () {
+    const thisApp = this;
 
-    init: function(){
-      const thisApp = this;
-      console.log('*** App starting ***');
-      console.log('thisApp:', thisApp);
-      console.log('classNames:', classNames);
-      console.log('settings:', settings);
-      console.log('templates:', templates);
+    thisApp.data = dataSource;
+    console.log('thisApp.data:', thisApp.data);
+  },
 
-      thisApp.initData();
-      thisApp.initMenu();
-    },
-  };
+  init: function () {
+    const thisApp = this;
+    console.log('*** App starting ***');
+    console.log('thisApp:', thisApp);
+    console.log('classNames:', classNames);
+    console.log('settings:', settings);
+    console.log('templates:', templates);
 
-  
-   // Uruchomienie aplikacji
-   app.init();
- 
+    thisApp.initData();
+    thisApp.initMenu();
+  },
+};
 
-
-
+// Uruchomienie aplikacji
+app.init();
